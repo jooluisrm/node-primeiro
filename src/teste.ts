829c37af94fcb1c1d0fd70178cc1b6aef612c5ef
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-19 09:54:40 -0300

    Uso de variáveis de ambiente

--- src/teste.ts
+++ src/teste.ts
@@ -1,4 +1,6 @@
 let nome: string = "joao luis";
 let idade: number = 19;
+let token: string = process.env.GOOGLE_DRIVE_TOKEN as string;
 
 console.log(`Meu nome e ${nome} e eu tenho ${idade} anos.`);
+console.log(`Conectando no Drive com o token: ${token}`);
