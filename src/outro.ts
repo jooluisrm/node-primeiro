fba329039c1c7d44b3a79cb1f69beadbb12729d5
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-19 09:36:05 -0300

    Modo Watch do Node

---
+++ src/outro.ts
@@ -0,0 +1,4 @@
+let versao: number = 5;
+
+
+console.log(`Ok na versão ${versao}`);
--- src/teste.ts
+++ src/teste.ts
@@ -1,4 +1,4 @@
 let nome: string = "joao luis";
-let idade: number = 21;
+let idade: number = 19;
 
 console.log(`Meu nome e ${nome} e eu tenho ${idade} anos.`);
