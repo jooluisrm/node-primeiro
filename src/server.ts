8cb5813e7cf1a1acec002ddd2bc84d1f792c6bb3
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-20 09:19:19 -0300

    Configuração básica do Express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express  from "express";
+import helmet from "helmet";
+import path from "path";
 
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, '../public')));
+
 
 server.get('/', (req, res) => {
     let name = "João Luís";
     let idade = 19;
-    res.send(`Olá ${name} eu tenho ${idade} anos`);
+    res.json([{name, idade}]);
 });
 
 server.listen(3000, () => {
