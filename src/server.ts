91955495ed39004e34a7c64bd56c7c3eed89df12
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-20 09:47:34 -0300

    Criação de rotas no Express 2

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,25 @@ server.get("/produtos", (req, res) => {
     res.json({ produtos: [] });
 });
 
+server.get("/produtos/:id", (req, res) => {
+    const { id } = req.params;
+
+    res.json({ id, name: "Teclado XYZ", price: 90 });
+});
+
+server.get("/voos/:from/:to", (req, res) => {
+
+    const { from, to } = req.params;
+
+    res.json({
+        voo: {
+            from: from.toUpperCase(),
+            to: to.toUpperCase(),
+            price: 12345
+        }
+    });
+})
+
 server.get('/', (req, res) => {
     let name = "João Luís";
     let idade = 19;
