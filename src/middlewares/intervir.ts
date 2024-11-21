c86cc5a66255c4706c83a22ee6edf9cdba644290
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-21 09:39:44 -0300

    Entendendo Middleware

---
+++ src/middlewares/intervir.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from "express";
+
+export const interferir: RequestHandler = (req, res, next) => {
+    let logged = true;
+
+    if(logged) {
+        next();
+    } else {
+        res.status(403).json({
+            error: "Middlewere nao permitiu"
+        })
+    }
+  
+}
