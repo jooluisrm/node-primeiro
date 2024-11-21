4e0283d7df30e7a6542a5bedf8b3374ab1dd1ddc
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-21 10:13:12 -0300

    Escrevendo arquivos (FS Module)

--- src/file.ts
+++ src/file.ts
@@ -1,5 +1,13 @@
+import { writeFile } from "fs/promises";
+
 const exec = async () => {
-    
+    console.log("Escrevendo no arquivo...");
+
+    const list = ["Bonieky", "João", "Pedro"];
+    const listTxt = list.join("\n");
+
+    await writeFile("./teste.txt", listTxt);
+    console.log("Pronto");
 }
 
 exec();
