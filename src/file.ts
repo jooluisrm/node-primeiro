a80edf893ac4af299d6bd76c76e2a91bde95e863
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-21 10:26:44 -0300

    Excluindo arquivos (FS Module)

--- src/file.ts
+++ src/file.ts
@@ -1,16 +1,7 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, unlink, writeFile } from "fs/promises";
 
 const exec = async () => {
-    const fileName = "./teste.txt"
-    const FileContent = await readFile(fileName, {encoding: 'utf-8'});
-    
-    const list = FileContent.split("\n");
-
-    list.push("Fulano");
-
-    const listText = list.join("\n");
-
-    await writeFile("./teste.txt", listText);
+    await unlink('./teste.txt');
 }
 
 exec();
